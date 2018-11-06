6eb30545ec83065f210d963bfaf0d1142d3b21e6
Saloni <saloni08g@gmail.com>
2018-11-06 15:44:29 +0530

    Add accessibiltiy and update service worker

--- sw.js
+++ sw.js
@@ -2,6 +2,9 @@
 var CACHE_NAME = 'site-cache';
 var urlsToCache = [
 	'/',
+	'/index.html',
+	'/restaurant.html',
+	'data/restaurants.json',
 	'/css/styles.css',
 	'/css/styles-medium.css',
 	'/css/styles-large.css',
@@ -18,5 +21,34 @@ self.addEventListener('install', function(event) {
   	  	console.log('Opened cache');
   	  	return cache.addAll(urlsToCache);
   	  })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+        	function(response) {
+        		// Check if we received a valid response
+	            if(!response || response.status !== 200 || response.type !== 'basic') {
+	              return response;
+	            }
+	            var responseToCache = response.clone();
+
+	            caches.open(CACHE_NAME)
+	              .then(function(cache) {
+	                cache.put(event.request, responseToCache);
+	              });
+	            return response;  
+	        }
+    	)
+      }
+    )
   );
 });
